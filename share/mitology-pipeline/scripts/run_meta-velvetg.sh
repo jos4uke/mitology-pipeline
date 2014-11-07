02d448d3a616785cc9f6cf3c000606659f63f4f4
Joseph Tran <Joseph.Tran@versailles.inra.fr>
2014-11-07 11:58:27 +0100

    update config sections list: add paths

--- share/mitology-pipeline/scripts/run_meta-velvetg.sh
+++ share/mitology-pipeline/scripts/run_meta-velvetg.sh
@@ -50,7 +50,7 @@ declare -r VERSION="dev"
 # DEFAULTS
 NAMESPACE_DEFAULT="MITOLOGY"
 SCAFFOLD_DEFAULT="no"
-CONFIG_SECTIONS=("contig_assembler" "scaffolder" "velveth" "velvetg" "meta_velvetg")
+CONFIG_SECTIONS=("paths" "contig_assembler" "scaffolder" "velveth" "velvetg" "meta_velvetg")
 
 WORKING_DIR=$(pwd)
 DATE=$(date '+%F_%Hh%Mm%Ss')
