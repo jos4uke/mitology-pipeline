3c1ef609667d191d546f99e7b569282f4aee0c77
Joseph Tran <Joseph.Tran@versailles.inra.fr>
2014-11-09 23:48:29 +0100

    output length-weighted kmer coverage histogram to pdf

--- share/mitology-pipeline/scripts/R/kmer_cov_weighted_hist.rplot.R
+++ share/mitology-pipeline/scripts/R/kmer_cov_weighted_hist.rplot.R
@@ -25,15 +25,15 @@ T1<-Sys.time()
 is.installed <- function(mypkg) is.element(mypkg, installed.packages()[,1])
 tryCatch.W.E <- function(expr)
 {
-	    W <- NULL
-  w.handler <- function(w)
-	      { # warning handler
-			          W <<- w
-          invokeRestart("muffleWarning")
-		    }
-      list(value = withCallingHandlers(tryCatch(expr, error = function(e) e),
-									                        warning = w.handler),
-		       warning = W)
+	W <- NULL
+	w.handler <- function(w)
+	{ # warning handler
+		W <<- w
+		invokeRestart("muffleWarning")
+	}
+	list(value = withCallingHandlers(tryCatch(expr, error = function(e) e),
+										warning = w.handler),
+										warning = W)
 }
 
 ## load and install libraries
@@ -80,7 +80,7 @@ if (opt$help) {
 }
 
 ## test input file
-if(length(arguments$args) != 1) {
+if (length(arguments$args) != 1) {
     cat(paste("author:  Joseph Tran <Joseph.Tran@versailles.inra.fr>\n\nversion: ", version, "\n\n"))
     print_help(parser)
     cat("notes:\n\t1. Copyright (c) 2012 by Joseph Tran. This work is made available under the terms of the Creative Commons Attribution-ShareAlike 3.0 license, http://creativecommons.org/licenses/by-sa/3.0/.\n\n")
@@ -89,7 +89,7 @@ if(length(arguments$args) != 1) {
     file <- arguments$args
 }
 
-if( file.access(file) == -1) {
+if ( file.access(file) == -1) {
     stop(sprintf("Specified file ( %s ) does not exist", file))
 } else {
     file_text <- readLines(file)
@@ -99,4 +99,54 @@ if( file.access(file) == -1) {
 if ( opt$verbose ) {
     write("Arguments have been parsed successfully.\n", stderr())
 }
+
+## load stats file
+# (R) > data = read.table("out-dir/meta-velvetg.Graph2-stats.txt", header=TRUE) 
+# (R) > weighted.hist(data$short1_cov, data$lgth, breaks=seq(0, 200, by=1))
+
+if ( opt$verbose ) {
+    write("Load Graph2 stats data ... ", stderr())
+}
+
+warn_err <- tryCatch.W.E(read.table(file, header=TRUE))
+
+if (is.null(warn_err$warning) && is.null(warn_err$value$message))
+{
+    data <- warn_err$value
+    print(data[1:10,], file=stderr())
+    cat("... ", file=stderr())
+    write(paste(dim(data),"\n"), stderr())
+} else {
+    stop(paste(geterrmessage(), str(warn_err)))
+}
+
+## weighted histogram
+
+# outdir: ?
+outDir <- dirname(file)
+pdf(file=file.path(outDir, "length-weighted_kmer_cov_hist.rplot.pdf"),
+	   onefile=TRUE,
+	   title=paste(strsplit(basename(file),".", fixed=TRUE)[[1]][1], "length-weigthed k-mer coverage histogram", sep=" "),
+	   paper="special", height=11.7, width=16.5)
+
+weighted.hist(data$short1_cov, data$lgth, breaks=seq(0, 500, by=1), xlab="K-mer coverage", ylab="Length-weighted Frequency", col=c("red"), main=paste(strsplit(basename(file),".", fixed=TRUE)[[1]][1], "length-weigthed k-mer coverage histogram", sep=" "))
+
+dev.off()
+
+## execution time: end
+T2<-Sys.time()
+Tdiff= difftime(T2, T1)
+cat("Execution time : ", Tdiff,"seconds\n", file=stderr())
+
+
+
+
+
+
+
+
+
+
+
+
 
