77611273e1285b36b547880c2ac6e07700a83706
Joseph Tran <Joseph.Tran@versailles.inra.fr>
2014-11-19 14:31:42 +0100

    run nucmer on contiging results

--- bin/mitology-pipeline.sh
+++ bin/mitology-pipeline.sh
@@ -1444,34 +1444,42 @@ function run_nucmer() {
 }
 
 # vs CONTIGING
-### test
-#logger_debug "[$NUCMER] contiging dir: $CONTIGING_OUTDIR"
-#assemblerContigsRef="${assembly_output}[assembler_contigs]"
-#logger_info "[$NUCMER] assembler contigs file: ${!assemblerContigsRef}"
-#assembler_contigs="${!assemblerContigsRef}"
-#sample_dir="${assembler_contigs##${OUTPUT_DIR}/${ASSEMBLY_OUTDIR}/}"
-#logger_debug "[$NUCMER] sample dir: $sample_dir"
-#NUCMER_OUTDIR="$OUTPUT_DIR/$ALIGNMENTS_OUTDIR/"
-#exit 1
-### end test
-
-#assembler_contigs=$(find $OUTPUT_DIR/$ASSEMBLY_OUTDIR/*/contiging* -iname meta-velvetg.contigs.fa)
 assembler_contigs="${assembly_output}[assembler_contigs]"
 sample_id+="_scaffolding_no"
 sample_dir=$(dirname ${!assembler_contigs##$OUTPUT_DIR/$ASSEMBLY_OUTDIR/})
-NUCMER_OUTDIR=$OUTPUT_DIR/$ALIGNMENTS_OUTDIR/$NUCMER/$sample_dir
+logger_info "$[${NUCMER}_contiging] run $NUCMER on ${!assembler_contigs} ... "
+NUCMER_CONTIGING_OUTDIR=$OUTPUT_DIR/$ALIGNMENTS_OUTDIR/$NUCMER/$sample_dir
+createDir -n "$NUCMER_CONTIGING_OUTDIR" -t "${NUCMER}_contiging" -e "$ERROR_TMP" -d
 
-mkdir -p $NUCMER_OUTDIR
+for organite in mito chloro; do
+	case $organite in
+		mito)
+			REF_GENOME=${ga_mito_ref_path}
+			;;
+		chloro)
+			REF_GENOME=${ga_chloro_ref_path}
+			;;
+	esac
+	NUCMER_CONTIGING_ORGANITE=$NUCMER_CONTIGING_OUTDIR/$organite
+	NUCMER_CONTIGING_ORGANITE_ERROR=$NUCMER_CONTIGING_ORGANITE/${NUCMER}_${organite}.err
+	createDir -n "$NUCMER_CONTIGING_ORGANITE" -t "${NUCMER}_$organite" -e "$ERROR_TMP" -d
+	run_nucmer "$REF_GENOME" "$(realpath ${!assembler_contigs})" "$(realpath $NUCMER_CONTIGING_ORGANITE)" "${sample_id}_${organite}" 2>$NUCMER_CONTIGING_ORGANITE_ERROR
+	rtrn=$?
+	nucmer_failed_msg="[${NUCMER}_$organite] Failed. Errors occured while running $NUCMER on ${!assembler_contigs} against $organite genome"
+	exit_on_error "$NUCMER_CONTIGING_ORGANITE_ERROR" "$nucmer_failed_msg" "$rtrn" "$OUTPUT_DIR/$ALIGNMENTS_OUTDIR/$ALIGNMENTS_DEBUGF" $SESSION_TAG $EMAIL
+	logger_debug "[${NUCMER}_$organite] PWD: $(pwd)"
+	cd $WORKING_DIR
+done
 
 ## mito
-run_nucmer "$REF_MT" "$(realpath ${!assembler_contigs})" "$(realpath $NUCMER_OUTDIR)" "${sample_id}_mito" 
-echo "PWD: $(pwd)"
-cd $WORKING_DIR
+#run_nucmer "$REF_MT" "$(realpath ${!assembler_contigs})" "$(realpath $NUCMER_OUTDIR)" "${sample_id}_mito" 
+#echo "PWD: $(pwd)"
+#cd $WORKING_DIR
 
 ## chloro
-run_nucmer "$REF_PT" "$(realpath ${!assembler_contigs})" "$(realpath $NUCMER_OUTDIR)" "${sample_id}_chloro"
-echo "PWD: $(pwd)"
-cd $WORKING_DIR
+#run_nucmer "$REF_PT" "$(realpath ${!assembler_contigs})" "$(realpath $NUCMER_OUTDIR)" "${sample_id}_chloro"
+#echo "PWD: $(pwd)"
+#cd $WORKING_DIR
 
 # vs SCAFFOLDING
 #scaffolder_contigs=$(find $OUTPUT_DIR/$ASSEMBLY_OUTDIR/*/scaff* -iname meta-velvetg.contigs.fa)
